5725031d05f9c86adc282dc49646b38811ac5909
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-26 15:38:14 +0500

    1.0 version

--- ex34/Program.cs
+++ ex34/Program.cs
@@ -10,6 +10,53 @@ namespace ex34
     {
         static void Main(string[] args)
         {
+            const string CommandExit = "exit";
+            const string CommandSum = "sum";
+
+            bool isOpen = true;
+            List<int> numbers = new List<int>();
+
+            while (isOpen)
+            {
+                Console.SetCursorPosition(0, 15);
+                Console.WriteLine($"Введенные числа: ");
+
+                foreach (var number in numbers)
+                {
+                    Console.WriteLine(number);
+                }
+
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Введите число, команду sum или exit: ");
+                string input = Console.ReadLine();
+
+                if (input != CommandExit && input != CommandSum)
+                {
+                    numbers.Add(Convert.ToInt32(input));
+                }
+                else if (input == CommandSum)
+                {
+                    int sum = 0;
+
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        sum += numbers[i];
+                    }
+
+                    Console.WriteLine($"Сумма введенных чисел равна: {sum}");
+                }
+                else if (input == CommandExit)
+                {
+                    isOpen = false;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод...");
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
     }
 }
